858f6e4243eaa0950845389a1789a69c45ed8006
Paulo Tolentino <paulo@appwwwards.com>
2024-06-08 02:30:43 +0800

    feat: add tailwind config as pkg

---
+++ packages/tailwind-config/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    ".storybook/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/stories/*.stories.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
