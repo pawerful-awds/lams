01ea617846d173fcbafd0739c787c114db850f20
Paulo Tolentino <paulo@appwwwards.com>
2024-06-08 11:57:10 +0800

    feat(app): add tailwind config and redux pkgs

--- packages/tailwind-config/index.js
+++ packages/tailwind-config/index.js
@@ -2,6 +2,7 @@ module.exports = {
   mode: "jit",
   content: [
     ".storybook/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/stories/*.stories.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
