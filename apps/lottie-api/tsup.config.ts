d5ecc6bcf362bd481368d6d13e8901e665c9d52d
Paulo Tolentino <paulo@appwwwards.com>
2024-06-18 02:28:41 +0800

    chore: update dockerfile for backend api

--- apps/lottie-api/tsup.config.ts
+++ apps/lottie-api/tsup.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'tsup';
+import copy from 'esbuild-plugin-copy';
 
 export default defineConfig({
   entry: ['./src'],
   splitting: false,
   sourcemap: true,
   clean: true,
+  loader: {
+    '.graphql': 'text',
+  },
+  esbuildPlugins: [
+    copy({
+      // This plugin will copy all .graphql files to the dist folder
+      assets: {
+        from: ['./src/**/*.graphql'],
+        to: ['./'],
+      },
+    }),
+  ],
 });
